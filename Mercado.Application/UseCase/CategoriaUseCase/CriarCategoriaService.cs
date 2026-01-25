6570515e8c3df393788cc80e935cd5eeb9c5bfaa
Kaique Lopes <kaiquelopessal@gmail.com>
2026-01-25 16:28:33 -0300

    criação Setor/Categoria

---
+++ Mercado.Application/UseCase/CategoriaUseCase/CriarCategoriaService.cs
@@ -0,0 +1,38 @@
+﻿using Mercado.Application.Dtos.CategoriaDto;
+using Mercado.Domain.Interfaces.Repositorio;
+using Mercado.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercado.Application.UseCase.CategoriaUseCase
+{
+    public class CriarCategoriaService
+    {
+        private IRepositorioCategoria _repositoroCategoria;
+        private IRepositorioSetor _repositorioSetor;
+
+        public CriarCategoriaService(IRepositorioCategoria repositoroCategoria, IRepositorioSetor repositorioSetor)
+        {
+            this._repositoroCategoria = repositoroCategoria;
+            this._repositorioSetor = repositorioSetor;
+        }
+
+        public void Executar(CriarCategoriaDto dto)
+        {
+            var setor = _repositorioSetor.BuscarPorId(dto.SetorId);
+
+            if (setor == null)
+            {
+                throw new Exception("Setor Nao existe");
+            }
+
+            var categoria = new Categoria(dto.Nome, dto.Descricao, dto.SetorId);
+
+            _repositoroCategoria.Salvar(categoria);
+        }
+    }
+}
+
---
+++ Mercado.Application/UseCase/SetorUseCase/CriarSetorService.cs
@@ -0,0 +1,27 @@
+﻿using Mercado.Application.Dtos;
+using Mercado.Domain.Interfaces.Repositorio;
+using Mercado.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mercado.Application.UseCase.SetorUseCase
+{
+    public class CriarSetorService
+    {
+        private IRepositorioSetor _repositorio;
+        public CriarSetorService(IRepositorioSetor repositorio)
+        {
+            this._repositorio = repositorio;           
+        }
+
+        public void Executar(CriarSetorDto dto)
+        {
+            var setor = new Setor(dto.Nome, dto.Descricao);
+
+            _repositorio.Salvar(setor);
+        }
+    }
+}
